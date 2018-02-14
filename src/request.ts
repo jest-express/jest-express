1e3ee0f21a6abe24ed7cc074902764350130e166
James W. Lane <LaneJamesW@JohnDeere.com>
2018-02-14 13:11:47 -0600

    feat(Added more testable methods to Request):
    fix #3

--- src/request.ts
+++ src/request.ts
@@ -1,25 +1,167 @@
 declare const jest: any;
 
 export class Request {
-  public query: any;
+  // Properties
+  public baseUrl: string;
+  public body: string;
+  public cookies: any;
+  public fresh: boolean;
+  public hostname: string;
+  public ip: string;
+  public ips: string[];
+  public method: string;
+  public originalUrl: string;
   public params: any;
+  public path: string;
+  public protocol: string;
+  public query: any;
+  public route: any;
+  public secure: boolean;
+  public signedCookies: any;
+  public stale: boolean;
+  public subdomains: string[];
+  public xhr: boolean;
+  // Methods
+  public accepts: any;
+  public acceptsCharsets: any;
+  public acceptsEncodings: any;
+  public acceptsLanguages: any;
+  public get: any;
+  public is: any;
+  public range: any;
 
   constructor() {
-    this.query = {};
+    // Properties
+    this.baseUrl = '';
+    this.body = '';
+    this.cookies = {};
+    this.fresh = false;
+    this.hostname = '';
+    this.ip = '';
+    this.ips = [];
+    this.method = '';
+    this.originalUrl = '';
     this.params = {};
+    this.path = '';
+    this.protocol = '';
+    this.query = {};
+    this.route = {};
+    this.secure = false;
+    this.signedCookies = {};
+    this.stale = false;
+    this.subdomains = [];
+    this.xhr = false;
+    // Methods
+    this.accepts = jest.fn();
+    this.acceptsCharsets = jest.fn();
+    this.acceptsEncodings = jest.fn();
+    this.acceptsLanguages = jest.fn();
+    this.get = jest.fn();
+    this.is = jest.fn();
+    this.range = jest.fn();
     return this;
   }
 
   public resetMocked() {
-    this.query = {};
+    // Properties
+    this.baseUrl = '';
+    this.body = '';
+    this.cookies = {};
+    this.fresh = false;
+    this.hostname = '';
+    this.ip = '';
+    this.ips = [];
+    this.method = '';
+    this.originalUrl = '';
     this.params = {};
+    this.path = '';
+    this.protocol = '';
+    this.query = {};
+    this.route = {};
+    this.secure = false;
+    this.signedCookies = {};
+    this.stale = false;
+    this.subdomains = [];
+    this.xhr = false;
+    // Methods
+    this.accepts.mockReset();
+    this.acceptsCharsets.mockReset();
+    this.acceptsEncodings.mockReset();
+    this.acceptsLanguages.mockReset();
+    this.get.mockReset();
+    this.is.mockReset();
+    this.range.mockReset();
+  }
+
+  public setBaseUrl(value: string) {
+    this.baseUrl = value;
+  }
+
+  public setBody(value: string) {
+    this.body = value;
+  }
+
+  public setCookies(key: string, value: string) {
+    this.cookies[key] = value;
+  }
+
+  public setFresh(value: boolean) {
+    this.fresh = value;
+  }
+
+  public setIp(value: string) {
+    this.ip = value;
+  }
+
+  public setIps(value: string) {
+    this.ips.push(value);
+  }
+
+  public setMethod(value: string) {
+    this.method = value;
+  }
+
+  public setOriginalUrl(value: string) {
+    this.originalUrl = value;
+  }
+
+  public setParams(key: string, value: string) {
+    this.params[key] = value;
+  }
+
+  public setPath(value: string) {
+    this.path = value;
+  }
+
+  public setProtocol(value: string) {
+    this.protocol = value;
   }
 
   public setQuery(key: string, value: string) {
     this.query[key] = value;
   }
 
-  public setParams(key: string, value: string) {
-    this.params[key] = value;
+  public setRoute(key: string, value: string) {
+    this.route[key] = value;
+  }
+
+  public setSecure(value: boolean) {
+    this.secure = value;
+  }
+
+  public setSignedCookies(key: string, value: string) {
+    this.signedCookies[key] = value;
+  }
+
+  public setStale(value: boolean) {
+    this.stale = value;
+  }
+
+  public setSubdomains(value: string) {
+    this.subdomains.push(value);
+  }
+
+  public setXhr(value: boolean) {
+    this.xhr = value;
   }
 }
--- test/unit/request.test.ts
+++ test/unit/request.test.ts
@@ -14,13 +14,646 @@ describe('Express Request', () => {
     request.resetMocked();
   });
 
-  test('setQuery', () => {
-    request.setQuery('title', 'My Site');
-    expect(request.query['title']).toMatch('My Site');
+  describe('Test baseUrl', () => {
+    test('baseUrl should be empty by default', () => {
+      expect(request.baseUrl).toEqual('');
+    });
+
+    test('baseUrl should allow you to update it', () => {
+      const url1 = chance.url();
+      request.setBaseUrl(url1);
+      expect(request.baseUrl).toEqual(url1);
+
+      const url2 = chance.url();
+      request.setBaseUrl(url2);
+      expect(request.baseUrl).toEqual(url2);
+    });
+
+    test('baseUrl should be empty after reset', () => {
+      const url = chance.url();
+      request.setBaseUrl(url);
+
+      request.resetMocked();
+
+      expect(request.baseUrl).toEqual('');
+    });
   });
 
-  test('setParams', () => {
-    request.setParams('title', 'My Site');
-    expect(request.params['title']).toMatch('My Site');
+  describe('Test body', () => {
+    test('body should be empty by default', () => {
+      expect(request.body).toEqual('');
+    });
+
+    test('body should allow you to update it', () => {
+      const body1 = chance.string();
+      request.setBody(body1);
+      expect(request.body).toEqual(body1);
+
+      const body2 = chance.string();
+      request.setBody(body2);
+      expect(request.body).toEqual(body2);
+    });
+
+    test('body should be empty after reset', () => {
+      const body = chance.string();
+      request.setBody(body);
+
+      request.resetMocked();
+
+      expect(request.body).toEqual('');
+    });
+  });
+
+  describe('Test cookies', () => {
+    test('cookies should be empty by default', () => {
+      expect(request.cookies).toEqual({});
+    });
+
+    test('cookies should allow you to update it', () => {
+      const firstKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const firstValue = chance.string();
+      const secondKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const secondValue = chance.string();
+      request.setCookies(firstKey, firstValue);
+      expect(request.cookies).toHaveProperty(firstKey, firstValue);
+
+      request.setCookies(secondKey, secondValue);
+      expect(request.cookies).toHaveProperty(firstKey, firstValue);
+      expect(request.cookies).toHaveProperty(secondKey, secondValue);
+    });
+
+    test('cookies should be empty after reset', () => {
+      const key = chance.string();
+      const value = chance.string();
+      request.setCookies(key, value);
+
+      request.resetMocked();
+
+      expect(request.cookies).toEqual({});
+    });
+  });
+
+  describe('Test fresh', () => {
+    test('fresh should be false by default', () => {
+      expect(request.fresh).toEqual(false);
+    });
+
+    test('fresh should allow you to update it', () => {
+      request.setFresh(true);
+      expect(request.fresh).toEqual(true);
+
+      request.setFresh(false);
+      expect(request.fresh).toEqual(false);
+    });
+
+    test('fresh should be VALUE after reset', () => {
+      request.setFresh(true);
+
+      request.resetMocked();
+
+      expect(request.fresh).toEqual(false);
+    });
+  });
+
+  describe('Test ip', () => {
+    test('ip should be VALUE by default', () => {
+      expect(request.ip).toEqual('');
+    });
+
+    test('ip should allow you to update it', () => {
+      const firstValue = chance.ip();
+      request.setIp(firstValue);
+      expect(request.ip).toEqual(firstValue);
+
+      const secondValue = chance.ip();
+      request.setIp(secondValue);
+      expect(request.ip).toEqual(secondValue);
+    });
+
+    test('ip should be VALUE after reset', () => {
+      request.setIp('VALUE');
+      request.resetMocked();
+
+      expect(request.ip).toEqual('');
+    });
+  });
+
+  describe('Test ips', () => {
+    test('ips should be VALUE by default', () => {
+      expect(request.ips).toHaveLength(0);
+    });
+
+    test('ips should allow you to update it', () => {
+      const firstValue = chance.ip();
+      request.setIps(firstValue);
+
+      expect(request.ips).toHaveLength(1);
+      expect(request.ips).toContain(firstValue);
+
+      const secondValue = chance.ip();
+      request.setIps(secondValue);
+
+      expect(request.ips).toHaveLength(2);
+      expect(request.ips).toContain(firstValue);
+      expect(request.ips).toContain(secondValue);
+    });
+
+    test('ips should be VALUE after reset', () => {
+      const firstValue = chance.ip();
+      request.setIps(firstValue);
+
+      request.resetMocked();
+
+      expect(request.ips).toHaveLength(0);
+    });
+  });
+
+  describe('Test method', () => {
+    test('method should be VALUE by default', () => {
+      expect(request.method).toEqual('');
+    });
+
+    test('method should allow you to update it', () => {
+      const firstValue = chance.pickone(['GET', 'POST', 'PUT', 'DELETE']);
+      request.setMethod(firstValue);
+      expect(request.method).toEqual(firstValue);
+
+      const secondValue = chance.pickone(['GET', 'POST', 'PUT', 'DELETE']);
+      request.setMethod(secondValue);
+      expect(request.method).toEqual(secondValue);
+    });
+
+    test('method should be VALUE after reset', () => {
+      const value = chance.pickone(['GET', 'POST', 'PUT', 'DELETE']);
+      request.setMethod(value);
+
+      request.resetMocked();
+
+      expect(request.method).toEqual('');
+    });
+  });
+
+  describe('Test originalUrl', () => {
+    test('originalUrl should be empty by default', () => {
+      expect(request.originalUrl).toEqual('');
+    });
+
+    test('originalUrl should allow you to update it', () => {
+      const firstValue = chance.url();
+      request.setOriginalUrl(firstValue);
+      expect(request.originalUrl).toEqual(firstValue);
+
+      const secondValue = chance.url();
+      request.setOriginalUrl(secondValue);
+      expect(request.originalUrl).toEqual(secondValue);
+    });
+
+    test('originalUrl should be empty after reset', () => {
+      const value = chance.url();
+      request.setOriginalUrl(value);
+
+      request.resetMocked();
+
+      expect(request.originalUrl).toEqual('');
+    });
+  });
+
+  describe('Test params', () => {
+    test('params should be empty by default', () => {
+      expect(request.params).toEqual({});
+    });
+
+    test('params should allow you to update it', () => {
+      const firstKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const firstValue = chance.string();
+      const secondKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const secondValue = chance.string();
+      request.setParams(firstKey, firstValue);
+      expect(request.params).toHaveProperty(firstKey, firstValue);
+
+      request.setParams(secondKey, secondValue);
+      expect(request.params).toHaveProperty(firstKey, firstValue);
+      expect(request.params).toHaveProperty(secondKey, secondValue);
+    });
+
+    test('params should be empty after reset', () => {
+      const key = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const value = chance.string();
+      request.setParams(key, value);
+
+      request.resetMocked();
+
+      expect(request.params).toEqual({});
+    });
+  });
+
+  describe('Test path', () => {
+    test('path should be empty by default', () => {
+      expect(request.path).toEqual('');
+    });
+
+    test('path should allow you to update it', () => {
+      const firstValue = chance.string();
+      request.setPath(firstValue);
+      expect(request.path).toEqual(firstValue);
+
+      const secondValue = chance.string();
+      request.setPath(secondValue);
+      expect(request.path).toEqual(secondValue);
+    });
+
+    test('path should be empty after reset', () => {
+      const value = chance.string();
+      request.setPath(value);
+
+      request.resetMocked();
+
+      expect(request.path).toEqual('');
+    });
+  });
+
+  describe('Test protocol', () => {
+    test('protocol should be empty by default', () => {
+      expect(request.protocol).toEqual('');
+    });
+
+    test('protocol should allow you to update it', () => {
+      const firstValue = chance.string();
+      request.setProtocol(firstValue);
+      expect(request.protocol).toEqual(firstValue);
+
+      const secondValue = chance.string();
+      request.setProtocol(secondValue);
+      expect(request.protocol).toEqual(secondValue);
+    });
+
+    test('protocol should be empty after reset', () => {
+      const value = chance.string();
+      request.setProtocol(value);
+
+      request.resetMocked();
+
+      expect(request.protocol).toEqual('');
+    });
+  });
+
+  describe('Test query', () => {
+    test('query should be empty by default', () => {
+      expect(request.query).toEqual({});
+    });
+
+    test('query should allow you to update it', () => {
+      const firstKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const firstValue = chance.string();
+      request.setQuery(firstKey, firstValue);
+      expect(request.query).toHaveProperty(firstKey, firstValue);
+
+
+      const secondKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const secondValue = chance.string();
+      request.setQuery(secondKey, secondValue);
+      expect(request.query).toHaveProperty(firstKey, firstValue);
+      expect(request.query).toHaveProperty(secondKey, secondValue);
+    });
+
+    test('query should be empty after reset', () => {
+      const key = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const value = chance.string();
+      request.setQuery(key, value);
+
+      request.resetMocked();
+
+      expect(request.query).toEqual({});
+    });
+  });
+
+  describe('Test route', () => {
+    test('route should be empty by default', () => {
+      expect(request.route).toEqual({});
+    });
+
+    test('route should allow you to update it', () => {
+      const userValue = chance.string();
+      const authenticatedValue = chance.bool();
+      request.setRoute('user', userValue);
+      expect(request.route).toHaveProperty('user', userValue);
+
+      request.setRoute('authenticated', authenticatedValue);
+      expect(request.route).toHaveProperty('user', userValue);
+      expect(request.route).toHaveProperty('authenticated', authenticatedValue);
+    });
+
+    test('route should be VALUE after reset', () => {
+      const userValue = chance.string();
+      request.setRoute('user', userValue);
+      request.resetMocked();
+
+      expect(request.route).toEqual({});
+    });
+  });
+
+  describe('Test secure', () => {
+    test('secure should be false by default', () => {
+      expect(request.secure).toEqual(false);
+    });
+
+    test('secure should allow you to update it', () => {
+      const firstValue = chance.bool();
+      request.setSecure(firstValue);
+      expect(request.secure).toEqual(firstValue);
+
+      const secondValue = chance.bool();
+      request.setSecure(secondValue);
+      expect(request.secure).toEqual(secondValue);
+    });
+
+    test('secure should be false after reset', () => {
+      const value = chance.bool();
+      request.setSecure(value);
+
+      request.resetMocked();
+
+      expect(request.secure).toEqual(false);
+    });
+  });
+
+  describe('Test signedCookies', () => {
+    test('signedCookies should be empty by default', () => {
+      expect(request.signedCookies).toEqual({});
+    });
+
+    test('signedCookies should allow you to update it', () => {
+      const firstKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const firstValue = chance.string();
+      const secondKey = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const secondValue = chance.string();
+
+      request.setSignedCookies(firstKey, firstValue);
+      expect(request.signedCookies).toHaveProperty(firstKey, firstValue);
+
+      request.setSignedCookies(secondKey, secondValue);
+      expect(request.signedCookies).toHaveProperty(firstKey, firstValue);
+      expect(request.signedCookies).toHaveProperty(secondKey, secondValue);
+    });
+
+    test('signedCookies should be empty after reset', () => {
+      const key = chance.string({ pool: 'abcdefghijklmnopqrstuvwxyz' });
+      const value = chance.string();
+      request.setSignedCookies(key, value);
+
+      request.resetMocked();
+
+      expect(request.signedCookies).toEqual({});
+    });
+  });
+
+  describe('Test stale', () => {
+    test('stale should be false by default', () => {
+      expect(request.stale).toEqual(false);
+    });
+
+    test('stale should allow you to update it', () => {
+      const fisrtValue = chance.bool();
+      request.setStale(fisrtValue);
+      expect(request.stale).toEqual(fisrtValue);
+
+      const secondValue = chance.bool();
+      request.setStale(secondValue);
+      expect(request.stale).toEqual(secondValue);
+    });
+
+    test('stale should be false after reset', () => {
+      const value = chance.bool();
+      request.setStale(value);
+
+      request.resetMocked();
+
+      expect(request.stale).toEqual(false);
+    });
+  });
+
+  describe('Test subdomains', () => {
+    test('subdomains should be empty by default', () => {
+      expect(request.subdomains).toHaveLength(0);
+    });
+
+    test('subdomains should allow you to update it', () => {
+      const firstValue = chance.string();
+      request.setSubdomains(firstValue);
+
+      expect(request.subdomains).toHaveLength(1);
+      expect(request.subdomains).toContain(firstValue);
+
+      const secondValue = chance.string();
+      request.setSubdomains(secondValue);
+
+      expect(request.subdomains).toHaveLength(2);
+      expect(request.subdomains).toContain(firstValue);
+      expect(request.subdomains).toContain(secondValue);
+    });
+
+    test('subdomains should be VALUE after reset', () => {
+      const value = chance.string();
+      request.setSubdomains(value);
+
+      request.resetMocked();
+
+      expect(request.subdomains).toHaveLength(0);
+    });
+  });
+
+  describe('Test xhr', () => {
+    test('xhr should be false by default', () => {
+      expect(request.xhr).toEqual(false);
+    });
+
+    test('xhr should allow you to update it', () => {
+      const firstValue = chance.bool();
+      request.setXhr(firstValue);
+      expect(request.xhr).toEqual(firstValue);
+
+      const secondValue = chance.bool();
+      request.setXhr(secondValue);
+      expect(request.xhr).toEqual(secondValue);
+    });
+
+    test('xhr should be false after reset', () => {
+      const value = chance.bool();
+      request.setXhr(value);
+
+      request.resetMocked();
+
+      expect(request.xhr).toEqual(false);
+    });
+  });
+
+  describe('Test accepts', () => {
+    test('accepts should have no calls by default', () => {
+      expect(request.accepts).not.toHaveBeenCalled();
+    });
+
+    test('accepts should be called and match called with', () => {
+      const types = chance.string();
+
+      request.accepts(types);
+
+      expect(request.accepts).toHaveBeenCalled();
+      expect(request.accepts).toHaveBeenCalledWith(types);
+    });
+
+    test('accepts should have no call after reset', () => {
+      const types = chance.string();
+      request.accepts(types);
+
+      request.resetMocked();
+
+      expect(request.accepts).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Test acceptsCharsets', () => {
+    test('acceptsCharsets should have no calls by default', () => {
+      expect(request.acceptsCharsets).not.toHaveBeenCalled();
+    });
+
+    test('acceptsCharsets should be called and match called with', () => {
+      const charset = chance.string();
+
+      request.acceptsCharsets(charset);
+
+      expect(request.acceptsCharsets).toHaveBeenCalled();
+      expect(request.acceptsCharsets).toHaveBeenCalledWith(charset);
+    });
+
+    test('acceptsCharsets should have no call after reset', () => {
+      const charset = chance.string();
+      request.acceptsCharsets(charset);
+
+      request.resetMocked();
+
+      expect(request.acceptsCharsets).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Test acceptsEncodings', () => {
+    test('acceptsEncodings should have no calls by default', () => {
+      expect(request.acceptsEncodings).not.toHaveBeenCalled();
+    });
+
+    test('acceptsEncodings should be called and match called with', () => {
+      const encoding = chance.string();
+
+      request.acceptsEncodings(encoding);
+
+      expect(request.acceptsEncodings).toHaveBeenCalled();
+      expect(request.acceptsEncodings).toHaveBeenCalledWith(encoding);
+    });
+
+    test('acceptsEncodings should have no call after reset', () => {
+      const encoding = chance.string();
+      request.acceptsEncodings(encoding);
+
+      request.resetMocked();
+
+      expect(request.acceptsEncodings).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Test acceptsLanguages', () => {
+    test('acceptsLanguages should have no calls by default', () => {
+      expect(request.acceptsLanguages).not.toHaveBeenCalled();
+    });
+
+    test('acceptsLanguages should be called and match called with', () => {
+      const lang = chance.string();
+
+      request.acceptsLanguages(lang);
+
+      expect(request.acceptsLanguages).toHaveBeenCalled();
+      expect(request.acceptsLanguages).toHaveBeenCalledWith(lang);
+    });
+
+    test('acceptsLanguages should have no call after reset', () => {
+      const lang = chance.string();
+      request.acceptsLanguages(lang);
+
+      request.resetMocked();
+
+      expect(request.acceptsLanguages).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Test get', () => {
+    test('get should have no calls by default', () => {
+      expect(request.get).not.toHaveBeenCalled();
+    });
+
+    test('get should be called and match called with', () => {
+      const field = chance.string();
+
+      request.get(field);
+
+      expect(request.get).toHaveBeenCalled();
+      expect(request.get).toHaveBeenCalledWith(field);
+    });
+
+    test('get should have no call after reset', () => {
+      const field = chance.string();
+      request.get(field);
+
+      request.resetMocked();
+
+      expect(request.get).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Test is', () => {
+    test('is should have no calls by default', () => {
+      expect(request.is).not.toHaveBeenCalled();
+    });
+
+    test('is should be called and match called with', () => {
+      const type = chance.string();
+
+      request.is(type);
+
+      expect(request.is).toHaveBeenCalled();
+      expect(request.is).toHaveBeenCalledWith(type);
+    });
+
+    test('is should have no call after reset', () => {
+      const type = chance.string();
+      request.is(type);
+
+      request.resetMocked();
+
+      expect(request.is).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Test range', () => {
+    test('range should have no calls by default', () => {
+      expect(request.range).not.toHaveBeenCalled();
+    });
+
+    test('range should be called and match called with', () => {
+      const size = chance.natural();
+      const options = {};
+
+      request.range(size, options);
+
+      expect(request.range).toHaveBeenCalled();
+      expect(request.range).toHaveBeenCalledWith(size, options);
+    });
+
+    test('range should have no call after reset', () => {
+      const size = chance.natural();
+      const options = {};
+      request.range(size, options);
+
+      request.resetMocked();
+
+      expect(request.range).not.toHaveBeenCalled();
+    });
   });
 });
