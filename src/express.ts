a9d9f1fe2ebebb5a4dfa9e9480739b64b19a1c3d
James W. Lane <LaneJamesW@JohnDeere.com>
2018-02-14 13:33:37 -0600

    feat(Added more testable methods to Express):

--- src/express.ts
+++ src/express.ts
@@ -4,16 +4,63 @@ import { Response } from './response';
 declare const jest: any;
 
 export class Express {
-  public request: any;
-  public response: any;
+  // Properties
+  public locals: any;
+  public mountpath: string;
+  // Methods
+  public all: any;
+  public delete: any;
+  public disable: any;
+  public disabled: any;
+  public enable: any;
+  public enabled: any;
+  public engine: any;
   public get: any;
+  public listen: any;
+  public head: any;
+  public post: any;
+  public put: any;
+  public connect: any;
+  public options: any;
+  public trace: any;
+  public patch: any;
+  public param: any;
+  public path: any;
+  public render: any;
   public route: any;
   public set: any;
   public use: any;
+  // Class
+  public request: Request;
+  public response: Response;
+  // Private
   private setting: any;
 
   constructor() {
+    // Properties
+    this.locals = {};
+    this.mountpath = '';
     this.setting = {};
+    // Methods
+    this.all = jest.fn();
+    this.delete = jest.fn();
+    this.disable = jest.fn();
+    this.disabled = jest.fn();
+    this.enable = jest.fn();
+    this.enabled = jest.fn();
+    this.engine = jest.fn();
+    this.listen = jest.fn();
+    this.head = jest.fn();
+    this.post = jest.fn();
+    this.put = jest.fn();
+    this.connect = jest.fn();
+    this.options = jest.fn();
+    this.trace = jest.fn();
+    this.patch = jest.fn();
+    this.param = jest.fn();
+    this.path = jest.fn();
+    this.render = jest.fn();
+    this.use = jest.fn();
     this.request = new Request();
     this.response = new Response();
     this.get = jest.fn((path: any, callback: any) => {
@@ -25,25 +72,58 @@ export class Express {
       }
       return path(this.request, this.response);
     });
+
     this.route = jest.fn(() => {
       return {
         get: this.get,
       };
     });
+
     this.set = jest.fn((key: any, value: any) => {
       this.setting[key] = value;
     });
-    this.use = jest.fn();
     return this;
   }
 
+  public setMountPath(value: string) {
+    this.mountpath = value;
+  }
+
+  public setLocals(key: string, value: string) {
+    this.locals[key] = value;
+  }
+
   public resetMocked() {
-    this.setting = {};
-    this.request.resetMocked();
-    this.response.resetMocked();
-    this.set.mockReset();
+    // Properties
+    this.locals = {};
+    this.mountpath = '';
+    // Methods
+    this.all.mockReset();
+    this.delete.mockReset();
+    this.disable.mockReset();
+    this.disabled.mockReset();
+    this.enable.mockReset();
+    this.enabled.mockReset();
+    this.engine.mockReset();
+    this.listen.mockReset();
+    this.head.mockReset();
+    this.post.mockReset();
+    this.put.mockReset();
+    this.connect.mockReset();
+    this.options.mockReset();
+    this.trace.mockReset();
+    this.patch.mockReset();
+    this.param.mockReset();
+    this.path.mockReset();
+    this.render.mockReset();
+    this.use.mockReset();
     this.get.mockReset();
     this.route.mockReset();
-    this.use.mockReset();
+    this.set.mockReset();
+    // Class
+    this.request.resetMocked();
+    this.response.resetMocked();
+    // Private
+    this.setting = {};
   }
 }
--- test/unit/express.test.ts
+++ test/unit/express.test.ts
@@ -14,6 +14,58 @@ describe('Express Application', () => {
     app.resetMocked();
   });
 
+  describe('Test locals', () => {
+    test('locals should be empty by default', () => {
+      expect(app.locals).toEqual({});
+    });
+
+    test('locals should allow you to update it', () => {
+      const userValue = chance.string();
+      const authenticatedValue = chance.bool();
+      app.setLocals('user', userValue);
+      expect(app.locals).toHaveProperty('user', userValue);
+
+      app.setLocals('authenticated', authenticatedValue);
+      expect(app.locals).toHaveProperty('user', userValue);
+      expect(app.locals).toHaveProperty('authenticated', authenticatedValue);
+    });
+
+    test('locals should be empty after reset', () => {
+      const userValue = chance.string();
+      app.setLocals('user', userValue);
+
+      app.resetMocked();
+
+      expect(app.locals).toEqual({});
+    });
+  });
+
+  describe('Test mountpath', () => {
+    test('mountpath should be empty by default', () => {
+      expect(app.mountpath).toEqual('');
+    });
+
+    test('mountpath should allow you to update it', () => {
+      const firstValue = chance.string();
+      app.setMountPath(firstValue);
+      expect(app.mountpath).toEqual(firstValue);
+
+      const secondValue = chance.string();
+      app.setMountPath(secondValue);
+      expect(app.mountpath).toEqual(secondValue);
+    });
+
+    test('mountpath should be empty after reset', () => {
+      const value = chance.string();
+      app.setMountPath(value);
+
+      app.resetMocked();
+
+      expect(app.mountpath).toEqual('');
+    });
+  });
+
+
   test('Get', () => {
     app.get('/', (request: any, response: any) => {
       expect(app.get).toHaveBeenCalled();
