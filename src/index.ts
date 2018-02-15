e8d09c61fa7054a2a91bbdfda5a72219290e8580
James W. Lane <james.w.lane@mac.com>
2018-02-15 09:52:58 -0600

    feat(Express): Added a default export function to be called

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { Express } from "./express";
+
 export { Express } from './express';
 export { Request } from './request';
 export { Response } from './response';
 export { Router } from './router';
+export default function () {
+  return new Express();
+}
