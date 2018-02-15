5dc0df8c228e647b5ebdfa817564f978818d30a4
James W. Lane <james.w.lane@mac.com>
2018-02-15 12:23:06 -0600

    fix(application): Fixed module.exports of the application

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 'use strict';
-var Express = require('./lib/express').Express;
 var Request = require('./lib/request').Request;
 var Response = require('./lib/response').Response;
 var Router = require('./lib/router').Router;
+var Express = require('./lib/express').Express;
 var Application = require('./lib/application').Application;
+
+module.exports.Request = Request;
+module.exports.Response = Response;
+module.exports.Router = Router;
 
 function createApplication() {
   return new Express();
 }
 
-var application = new Application();
+module.exports = createApplication;
 
-exports.Request = Request;
-exports.Response = Response;
-exports.Router = Router;
-exports.json = application.json;
-exports.query = application.query;
-exports.static = application.staticLoad;
-exports.urlencoded = application.urlencoded;
-exports.resetApplication = application.resetMocked;
-exports = module.exports = createApplication;
+var application = new Application();
+module.exports.json = application.json;
+module.exports.query = application.query;
+module.exports.static = application.staticLoad;
+module.exports.urlencoded = application.urlencoded;
+module.exports.resetApplication = application.resetMocked;
