fb86170afa1a28dcf1bbcd5ed1387868eb6e3e9b
Jason Seminara <js@ga.co>
2018-07-28 14:59:33 -0400

    Creates Router.route.[get,put,post,patch,delete]
    adds tests
    
    fixed bug that was mixing up 'this' scope; replaced with a closure
    
    mock functions were colliding

--- test/unit/router.test.ts
+++ test/unit/router.test.ts
@@ -34,23 +34,45 @@ describe('Express Router', () => {
     });
   });
 
+  test('get without path', () => {
+    router.get((request: Request, response: Response, next: NextFunction) => {
+      expect(router.get).toHaveBeenCalled();
+      expect(router.get).toHaveBeenCalledWith(expect.anything());
+      expect(request).toBeInstanceOf(Request);
+      expect(response).toBeInstanceOf(Response);
+      expect(response).toBeInstanceOf(Response);
+      expect(next).toEqual(next);
+    });
+  });
+
   test('param', () => {
     router.param('title');
     expect(router.param).toHaveBeenCalled();
     expect(router.param).toHaveBeenCalledWith('title');
   });
 
-  test('Route', () => {
-    router.route('/events')
-      .get((request: any, response: any) => {
-        expect(router.route).toHaveBeenCalled();
-        expect(router.route).toHaveBeenCalledWith('/events');
-        expect(router.get).toHaveBeenCalled();
-        expect(router.get).toHaveBeenCalledWith(expect.anything());
-        expect(request).toBeInstanceOf(Request);
-        expect(response).toBeInstanceOf(Response);
+  describe('Route', () => {
+    let path:string = '/events';
+
+    beforeAll(() => {
+      path = '/events';
+    });
+
+    ['get','post', 'put', 'patch', 'delete'].forEach((verb:string) => {
+      test(verb, () => {
+        router.route(path)[verb]((request: any, response: any) => {
+            expect(router.route).toHaveBeenCalled();
+            expect(router.route).toHaveBeenCalledWith(path);
+            expect(router[verb]).toHaveBeenCalled();
+            expect(router[verb]).toHaveBeenCalledWith(path, expect.any(Function));
+            expect(request).toBeInstanceOf(Request);
+            expect(response).toBeInstanceOf(Response);
+          });
       });
+    });
+
   });
+
 
   test('use', () => {
     router.use('/events');
